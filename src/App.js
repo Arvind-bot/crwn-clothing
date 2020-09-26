5d85ed3f7421a3d318938323c95de54a0d1c6a45
Arvind Kumar <arvindof7@gmail.com>
2020-09-26 16:00:59 +0530

    finishing signin handleSubmit method

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends React.Component {
             },
           });
 
-          console.log(this.state);
+          // console.log(this.state);
         });
       }
       this.setState({ currentUser: userAuth });
