7118aeb21e4656c88c7dc6c213fa93fd5df7d23b
Arvind Kumar <arvindof7@gmail.com>
2020-09-30 12:35:56 +0530

    updating our app redirect to home if user is signed in

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
-import {connect} from "react-redux";
-import {setCurrentUser} from "./redux/user/user.actions"
+import { connect } from "react-redux";
+import { setCurrentUser } from "./redux/user/user.actions";
 // import { render } from "node-sass";
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
-  
+
   componentDidMount() {
-    const {setCurrentUser} =this.props;
+    const { setCurrentUser } = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
+
         userRef.onSnapshot((snapShot) => {
-            setCurrentUser({
-              id: snapShot.id,
-              ...snapShot.data(),
-            }); 
+          // console.log("onSnapshot called");
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data(),
+          });
+          // console.log("onSnapshot printed");
         });
       }
+      // console.log("UA called");
       setCurrentUser(userAuth);
+      // console.log("UA printed");
     });
   }
 
@@ -40,15 +45,29 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SignInAndSignUpPage} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to="/" />
+              ) : (
+                <SignInAndSignUpPage />
+              )
+            }
+          />
         </Switch>
       </div>
     );
   }
 }
 
-const mapDispatchToProps=dispatch=>({
-  setCurrentUser:user=>dispatch(setCurrentUser(user))
-})
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
 
-export default connect(null,mapDispatchToProps)(App);
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
