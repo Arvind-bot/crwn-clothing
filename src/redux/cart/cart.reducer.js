0fa7a01b519969de5505fe4eac07f772f00e2f3d
Arvind Kumar <arvindof7@gmail.com>
2020-10-12 21:20:36 +0530

    added addItem and removeItem functionality to CheckoutItem item component

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,29 +1,41 @@
-
 import CartActionTypes from "./cart.types";
-import {addItemToCart} from "./cart.utils";
+import { addItemToCart, removeItemFromCart } from "./cart.utils";
 
-const INITIAL_STATE={
-    hidden:true,
-    cartItems:[]
+const INITIAL_STATE = {
+  hidden: true,
+  cartItems: [],
 };
 
-const cartReducer=(state=INITIAL_STATE,actions)=>{
-    switch(actions.type){
-        case CartActionTypes.TOGGLE_CART_HIDDEN:
-            return{
-                ...state,
-                hidden:!state.hidden
-            }
-        default:
-            return state;
+const cartReducer = (state = INITIAL_STATE, actions) => {
+  switch (actions.type) {
+    case CartActionTypes.TOGGLE_CART_HIDDEN:
+      return {
+        ...state,
+        hidden: !state.hidden,
+      };
+    default:
+      return state;
 
-        case CartActionTypes.ADD_ITEM:
-            return{
-                ...state,
-                cartItems:addItemToCart(state.cartItems,actions.payload)
-            }
-    }
-}
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: addItemToCart(state.cartItems, actions.payload),
+      };
+
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          (cartItem) => cartItem.id !== actions.payload.id
+        ),
+      };
+
+      case CartActionTypes.REMOVE_ITEM:
+        return {
+          ...state,
+          cartItems: removeItemFromCart(state.cartItems,actions.payload),
+        };
+  }
+};
 
 export default cartReducer;
-
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -11,5 +11,21 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     );
   }
 
-  return [...cartItems,{...cartItemToAdd,quantity:1}];
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existing = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  );
+
+  if (existing.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== existing.id);
+  }
+
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
 };
